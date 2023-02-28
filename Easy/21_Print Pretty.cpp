549c62b82984de80517bbef2cecbffa38e9ae4f0
Pranay Kesava <pranaykesava@gmail.com>
2023-02-28 12:49:22 +0530

    Create 21_Print Pretty.cpp

---
+++ Easy/21_Print Pretty.cpp
@@ -0,0 +1,5 @@
+
+cout << left << showbase << nouppercase << hex << (long long)A << endl;
+cout << setprecision(2) << setw(15) << right << showpos << fixed <<setfill('_') << B << endl;  
+cout << setprecision(9) << noshowpos << uppercase << scientific << C << endl;
+
